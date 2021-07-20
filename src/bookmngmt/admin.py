d23326d4e90e7b7a658159f8ff65503dc408e41e
aflah <aflahvk2527@gmail.com>
2021-07-20 21:12:01 +0530

    Customized AdminSite

--- src/bookmngmt/admin.py
+++ src/bookmngmt/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Stock
+from .forms import StockCreateForm
 
 # Register your models here.
 
-admin.site.register(Stock)
+class StockCreateAdmin(admin.ModelAdmin):
+    list_display = ['course', 'semester', 'book_name', 'book_count']
+    form = StockCreateForm
+    list_filter = ['course','semester']
+    search_fields = ['course', 'semester', 'book_name']
+
+admin.site.register(Stock, StockCreateAdmin)
---
+++ src/bookmngmt/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.forms import fields
+from .models import Stock
+
+class StockCreateForm(forms.ModelForm):
+    class Meta:
+        model = Stock
+        fields = ['course', 'semester', 'book_name','book_count','book_cover', 'index_page']
