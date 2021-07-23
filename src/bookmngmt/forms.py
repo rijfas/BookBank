2ccf6364caf0b92458200c71fd0b85b61e1e9374
aflah <aflahvk2527@gmail.com>
2021-07-24 00:53:22 +0530

    Form Validation

--- src/bookmngmt/forms.py
+++ src/bookmngmt/forms.py
@@ -6,6 +6,24 @@ class StockCreateForm(forms.ModelForm):
     class Meta:
         model = Stock
         fields = ['course', 'semester', 'book_name','book_count','book_cover', 'index_page']
+    def clean_course(self):
+        course = self.cleaned_data.get('course')
+        if not course:
+            raise forms.ValidationError('This field is required')
+            return course
+    def clean_semester(self):
+        semester = self.cleaned_data.get('semester')
+        if not semester:
+            raise forms.ValidationError('This field is required')
+            return semester
+    def clean_book_name(self):
+        book_name = self.cleaned_data.get('book_name')
+        if not book_name:
+            raise forms.ValidationError('This field is required')
+        for instance in Stock.objects.all():
+            if instance.book_name == book_name:
+                raise forms.ValidationError(str(book_name) + ' is already added')
+        return book_name
 
 class StockSearchForm(forms.ModelForm):
    class Meta:
